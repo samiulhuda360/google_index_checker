69f2eef9904b14c1ce66f713517f1e85b26bc5dd
samiulhuda360 <samidurbar@gmail.com>
2023-11-15 20:52:04 +0600

    initial commit

--- index_checker_project/settings.py
+++ index_checker_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-fmp&#v02kckbf(pvfm7%wle=+-43i%zwx8mr=i8lnstt435li&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
